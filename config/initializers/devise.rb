e029aef721d16afe84b1e12a4a4c986cbafcb224
mnipper <mjn4406@gmail.com>
2015-01-18 15:36:17 -0500

    Do not log secret_key_base

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -257,5 +257,3 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 end
-
-puts Rails.application.secrets.secret_key_base
